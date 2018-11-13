7fea633302c6c2093b8683bfb60a54c34f026d87
MatiasALopez <matutelopez@hotmail.com>
2018-11-13 11:29:46 -0300

    Actualizar PaginacionYOrdenamientoParametros

--- Source/namasdev.Core/Linq/PaginacionYOrdenamientoParametros.cs
+++ Source/namasdev.Core/Linq/PaginacionYOrdenamientoParametros.cs
@@ -8,9 +8,28 @@ namespace namasdev.Linq
 {
     public class PaginacionYOrdenamientoParametros
     {
-        public int? DesdeIndice { get; set; }
-        public int? CantMaximaRegistrosPorPagina { get; set; }
+        private int _pagina;
+        private int _cantMaximaRegistrosPorPagina;
+
+        public PaginacionYOrdenamientoParametros()
+        {
+            _pagina = _cantMaximaRegistrosPorPagina = 1;
+        }
+
+        public int Pagina
+        {
+            get { return _pagina; }
+            set { _pagina = Math.Max(value, 1); }
+        }
+
+        public int CantMaximaRegistrosPorPagina
+        {
+            get { return _cantMaximaRegistrosPorPagina; }
+            set { _cantMaximaRegistrosPorPagina = Math.Max(value, 1); }
+        }
+
         public int CantTotalRegistros { get; set; }
+
         public string ExpresionOrdenamiento { get; set; }
     }
 }
